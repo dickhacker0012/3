ed66a130cc9637c57714e8f855bcd066fd2b1512
dickhacker0012 <165778406+dickhacker0012@users.noreply.github.com>
2024-12-15 18:27:21 +0000

    test

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-Deno.serve(() => new Response("Hello, world!"));
+const testRoute = new URLPattern({ pathname: '/test' });
+
+function handler(req: Request): Response {
+    const match = testRoute.exec(req.url);
+
+    if (match) {
+        return new Response('Test!');
+    }
+    
+    return new Response("Hello, world!");
+};
+
+
+
+
+
+
+
+
+
+
+Deno.serve(handler);
